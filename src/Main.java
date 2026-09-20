282f78db2847bb329e1548dd3563a94da936aaef
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-20 17:35:13 -0300

    Arreglar

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,18 @@ public class Main {
         inventario.add("2- inventario");
         inventario.add("3- inventario");
 
-        System.out.println("Lista inventario: " + inventario);
 
+        for (int i = 0; i < inventario.size(); i++){
+            System.out.println("Indice: " + i + " - " + inventario.get(i));
+        }
+        inventario.set(1, "nuevoValor");
 
-        System.out.println("Cantidad de cursos: " + inventario.size());
+        System.out.println("Actualizacion");
+        System.out.println("Nuevo elemento: " + inventario.get(1));
+        System.out.println("Tamaño del inventario: " + inventario.size());
 
-
+        for ( int i = inventario.size() - 1;  i >= 0; i--){
+            System.out.println("Indice" + i +  "-" + inventario.get(i));
+        }
     }
 }
