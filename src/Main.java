8c613eb14254f8b261e33d0fba4a4a32b3a56b06
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-20 13:33:59 -0300

    feat: crear inventario con ArrayList

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+ import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> inventario = new ArrayList<>();
+
+
+        inventario.add("1- inventiario");
+        inventario.add("2- inventario");
+        inventario.add("3- inventario");
+
+        System.out.println("Lista inventario: " + inventario);
+
+
+        System.out.println("Cantidad de cursos: " + inventario.size());
+
+
+    }
+}
